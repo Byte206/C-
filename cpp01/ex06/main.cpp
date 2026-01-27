27c8ec80cf2ea0c57a88aed6317374d37d12e5a2
gabi <189730327+Byte206@users.noreply.github.com>
2026-01-27 14:36:49 +0100

    This should be ready

---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,47 @@
+#include "Harl.hpp"
+#include <iostream>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+        return 0;
+    }
+
+    Harl harl;
+    std::string input = argv[1];
+    int level = -1;
+
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for (int i = 0; i < 4; i++) {
+        if (levels[i] == input) {
+            level = i;
+            break;
+        }
+    }
+
+    switch (level) {
+        case 0:
+            std::cout << "[ DEBUG ]" << std::endl;
+            harl.complain("DEBUG");
+            std::cout << std::endl;
+            // fallthrough
+        case 1:
+            std::cout << "[ INFO ]" << std::endl;
+            harl.complain("INFO");
+            std::cout << std::endl;
+            // fallthrough
+        case 2:
+            std::cout << "[ WARNING ]" << std::endl;
+            harl.complain("WARNING");
+            std::cout << std::endl;
+            // fallthrough
+        case 3:
+            std::cout << "[ ERROR ]" << std::endl;
+            harl.complain("ERROR");
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    }
+
+    return 0;
+}
